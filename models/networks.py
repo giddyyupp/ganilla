ccb192926864a690280849da746f934d75e86cf5
nermin <nermin.samet@gmail.com>
2019-01-18 14:16:05 +0300

    fixed indent

--- models/networks.py
+++ models/networks.py
@@ -502,12 +502,12 @@ class BasicBlock_sam(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock_sam, self).__init__()
-        
-	self.rp1 = nn.ReflectionPad2d(1)
-	self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=0, bias=False)
+
+        self.rp1 = nn.ReflectionPad2d(1)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=0, bias=False)
         self.bn1 = nn.InstanceNorm2d(planes)
         self.rp2 = nn.ReflectionPad2d(1)
-	self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=0, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=0, bias=False)
         self.bn2 = nn.InstanceNorm2d(planes)
         self.out_planes = planes
 
