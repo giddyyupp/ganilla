13f785f63db427ad9403ddbea0f8fbf8d33d0507
nsamet <nermin.samet@gmail.com>
2019-02-12 15:53:47 +0300

    sacmalik

--- models/networks.py
+++ models/networks.py
@@ -97,8 +97,6 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
             net = resnet50(pretrained=False)
             # netG_B2A = resnet50(pretrained=False)
         # net = Resnet(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6)
-    elif netG == "ablation_model1":
-        net = AblationModel1(BasicBlock_sam, [2, 2, 2, 2])
     elif netG == 'unet_128':
         net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'unet_256':
@@ -504,6 +502,7 @@ class BasicBlock_sam(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock_sam, self).__init__()
+
         self.rp1 = nn.ReflectionPad2d(1)
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=0, bias=False)
         self.bn1 = nn.InstanceNorm2d(planes)
@@ -520,14 +519,14 @@ class BasicBlock_sam(nn.Module):
             )
 
             self.final_conv = nn.Sequential(
-                nn.ReflectionPad2d(1),
+		        nn.ReflectionPad2d(1),
                 nn.Conv2d(self.expansion * planes * 2, self.expansion * planes, kernel_size=3, stride=1,
                                         padding=0, bias=False),
                 nn.InstanceNorm2d(self.expansion * planes)
             )
         else:
             self.final_conv = nn.Sequential(
-                nn.ReflectionPad2d(1),
+		        nn.ReflectionPad2d(1),
                 nn.Conv2d(planes*2, planes, kernel_size=3, stride=1, padding=0, bias=False),
                 nn.InstanceNorm2d(planes)
             )
@@ -601,10 +600,10 @@ class PyramidFeatures(nn.Module):
         # add P4 elementwise to C3
         self.P3_1 = nn.Conv2d(C3_size, feature_size, kernel_size=1, stride=1, padding=0)
         self.P3_upsampled = nn.Upsample(scale_factor=2, mode='nearest')
-        self.rp3 = nn.ReflectionPad2d(1)
-        self.P3_2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, stride=1, padding=0)
+        #self.rp3 = nn.ReflectionPad2d(1)
+        #self.P3_2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, stride=1, padding=0)
 
-        #self.P2_1 = nn.Conv2d(C2_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P2_1 = nn.Conv2d(C2_size, feature_size, kernel_size=1, stride=1, padding=0)
         self.P2_upsampled = nn.Upsample(scale_factor=2, mode='nearest')
         self.rp4 = nn.ReflectionPad2d(1)
         self.P2_2 = nn.Conv2d(feature_size, feature_size/2, kernel_size=3, stride=1, padding=0)
@@ -616,7 +615,7 @@ class PyramidFeatures(nn.Module):
 
     def forward(self, inputs):
 
-        C3, C4, C5 = inputs
+        C2, C3, C4, C5 = inputs
 
         P5_x = self.P5_1(C5)
         P5_upsampled_x = self.P5_upsampled(P5_x)
@@ -632,12 +631,12 @@ class PyramidFeatures(nn.Module):
         P3_x = self.P3_1(C3)
         P3_x = P3_x + P4_upsampled_x
         P3_upsampled_x = self.P3_upsampled(P3_x)
-        P3_x = self.rp3(P3_upsampled_x)
-        P3_x = self.P3_2(P3_x)	
+        #P3_x = self.rp3(P3_x)
+        #P3_x = self.P3_2(P3_x)	
 
-        #P2_x = self.P2_1(C2)
-        #P2_x = P2_x + P3_upsampled_x
-        P2_upsampled_x = self.P2_upsampled(P3_x)
+        P2_x = self.P2_1(C2)
+        P2_x = P2_x + P3_upsampled_x
+        P2_upsampled_x = self.P2_upsampled(P2_x)
         P2_x = self.rp4(P2_upsampled_x)
         P2_x = self.P2_2(P2_x)
 
@@ -761,158 +760,11 @@ class ResNet(nn.Module):
         x3 = self.layer3(x2)
         x4 = self.layer4(x3)
 
-        out = self.fpn([x2, x3, x4])
+        out = self.fpn([x1, x2, x3, x4])
 
         out = self.pad3(out)
         out = self.conv2(out)
         out = self.tanh(out)
-
-        return out
-
-
-class AblationModel1(nn.Module):
-    def __init__(self, block, layers, ngf=64):
-        self.inplanes = 64
-        self.ngf = ngf
-        super(AblationModel1, self).__init__()
-
-        # first conv
-        self.pad1 = nn.ReflectionPad2d(3)
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=0, bias=True)
-        self.in1 = nn.InstanceNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.pad2 = nn.ReflectionPad2d(1)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
-
-        # Output layer
-        # self.pad3 = nn.ReflectionPad2d(3)
-        # self.conv2 = nn.Conv2d(64, 3, 7)
-        # self.tanh = nn.Tanh()
-
-        if block == BasicBlock_orj:
-            # residuals
-            self.layer1 = self._make_layer(block, 64, layers[0])
-            self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-            self.layer3 = self._make_layer(block, 128, layers[2], stride=2)
-            self.layer4 = self._make_layer(block, 256, layers[3], stride=2)
-
-            fpn_sizes = [self.layer1[layers[0] - 1].conv2.out_channels,
-                         self.layer2[layers[1] - 1].conv2.out_channels,
-                         self.layer3[layers[2] - 1].conv2.out_channels,
-                         self.layer4[layers[3] - 1].conv2.out_channels]
-
-        elif block == BasicBlock_sam:
-            # residuals
-            self.layer1 = self._make_layer_sam(block, 64, layers[0])
-            self.layer2 = self._make_layer_sam(block, 128, layers[1], stride=2)
-            self.layer3 = self._make_layer_sam(block, 128, layers[2], stride=2)
-            self.layer4 = self._make_layer_sam(block, 256, layers[3], stride=2)
-
-            fpn_sizes = [self.layer1[layers[0] - 1].conv2.out_channels,
-                         self.layer2[layers[1] - 1].conv2.out_channels,
-                         self.layer3[layers[2] - 1].conv2.out_channels,
-                         self.layer4[layers[3] - 1].conv2.out_channels]
-
-        elif block == Bottleneck:
-            # residuals
-            self.layer1 = self._make_layer(block, 64, layers[0])
-            self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-            self.layer3 = self._make_layer(block, 128, layers[2], stride=2)
-            self.layer4 = self._make_layer(block, 256, layers[3], stride=2)
-
-            fpn_sizes = [self.layer1[layers[0] - 1].conv3.out_channels,
-                         self.layer2[layers[1] - 1].conv3.out_channels,
-                         self.layer3[layers[2] - 1].conv3.out_channels,
-                         self.layer4[layers[3] - 1].conv3.out_channels]
-
-        # self.fpn = PyramidFeatures(fpn_sizes[0], fpn_sizes[1], fpn_sizes[2], fpn_sizes[3])
-
-        n_downsampling = 4
-
-        model = []
-
-        model += [nn.ReflectionPad2d(1)]
-        model += [nn.Conv2d(256, self.ngf * 2 ** (n_downsampling), kernel_size=3, padding=0)]
-        model += [nn.ReLU(inplace=True)]
-
-        for i in range(n_downsampling):
-            mult = 2 ** (n_downsampling - i)
-            model += [nn.ConvTranspose2d(self.ngf * mult, int(self.ngf * mult / 2),
-                                         kernel_size=3, stride=2,
-                                         padding=1, output_padding=1,
-                                         bias=False),
-                      nn.InstanceNorm2d(int(self.ngf * mult / 2)),
-                      nn.ReLU(True)]
-        model += [nn.ReflectionPad2d(3)]
-        model += [nn.Conv2d(self.ngf, 3, kernel_size=7, padding=0)]
-        model += [nn.Tanh()]
-
-        self.deconv_part = nn.Sequential(*model)
-
-        #for m in self.modules():
-        #    if isinstance(m, nn.Conv2d):
-        #        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-        #        m.weight.data.normal_(0, math.sqrt(2. / n))
-        #    elif isinstance(m, nn.BatchNorm2d):
-        #        m.weight.data.fill_(1)
-        #        m.bias.data.zero_()
-
-        # self.freeze_bn()
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=True),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def _make_layer_sam(self, block, planes, blocks, stride=1):
-        strides = [stride] + [1] * (blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.inplanes, planes, stride))
-            self.inplanes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def freeze_bn(self):
-        '''Freeze BatchNorm layers.'''
-        for layer in self.modules():
-            if isinstance(layer, nn.BatchNorm2d):
-                layer.eval()
-
-    def forward(self, inputs):
-
-        img_batch = inputs
-
-        x = self.pad1(img_batch)
-        x = self.conv1(x)
-        x = self.in1(x)
-        x = self.relu(x)
-        x = self.pad2(x)
-        x = self.maxpool(x)
-
-        x1 = self.layer1(x)
-        x2 = self.layer2(x1)
-        x3 = self.layer3(x2)
-        x4 = self.layer4(x3)
-
-        out = self.deconv_part(x4)
-
-        # out = self.fpn([x2, x3, x4])
-        #
-        # out = self.pad3(out)
-        # out = self.conv2(out)
-        # out = self.tanh(out)
 
         return out
 
