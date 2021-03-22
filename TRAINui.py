ea417fa6e86ca081546175198eac33ccae42d7e6
nathan d'angelo <nathandangeloo@gmail.com>
2021-03-22 13:55:00 +0000

    Update TRAINui.py
    added cancel button

--- TRAINui.py
+++ TRAINui.py
@@ -72,6 +72,23 @@ def openImageView(event, obj):
 # def openResultsDir:
 #     filedialog.askdirectory(initialdir ="./", title = "Select Folder Containing Model")
 
+<<<<<<< Updated upstream
+=======
+def cancel_convert():
+    print("Cancelling Conversion...")
+    global running
+    running = False
+    print("==============Cancelled=================")
+    progress_var.set(0)
+    
+def start_convert():
+    global running
+    running = True
+    thread = threading.Thread(target=convert)
+    print("Starting Conversion...")
+    thread.start()
+
+>>>>>>> Stashed changes
 def convert():
     if __name__ == '__main__':
         try:
@@ -140,6 +157,12 @@ window.geometry("750x575")
 window.resizable(False, False)
 window.configure(bg="white")
 
+<<<<<<< Updated upstream
+=======
+progress_var = DoubleVar()
+progressbar = ttk.Progressbar(variable=progress_var, length=280)
+
+>>>>>>> Stashed changes
 frameDatasetLabel = Frame()
 frameDatasetLabel.configure(bg="white")
 frameDataset = Frame()
@@ -167,7 +190,11 @@ outputBox = scrolledtext.ScrolledText(window,
 
 lblTitle = Label(window, text='GANILLA UI - TRAIN', font='Helvetica 20 bold', fg="white", bg="black", anchor='nw', width=40, height=1)
 lblSub = Label(window, text='TRAIN A MODEL WITH A DATASET...', font='Helvetica 10', fg="white", bg="black", anchor='nw', width=85, height=1)
+<<<<<<< Updated upstream
 lblFoot = Label(window, text='CREATED BY GM, CD & ND', font='Helvetica 10', fg="white", bg="black", anchor='nw', width=85, height=1)
+=======
+lblFoot = Label(window, text='CREATED BY GM, ND, & CD', font='Helvetica 10', fg="white", bg="black", anchor='nw', width=85, height=1)
+>>>>>>> Stashed changes
 
 lblName = Label(window, text='Model Name', font='Helvetica 10 bold', bg="white")
 txtName = Entry(window, width = 20, bg="white") #textVariable = modelName
@@ -193,9 +220,15 @@ txtEpoch = Entry(frameEpoch, width = 10, bg="white")
 lblResize = Label(window, text='Resize', font='Helvetica 10 bold', bg="white")
 drpResize = OptionMenu(frameResize, "resize_and_crop", "scale_width", "scale_width_and_crop", "none")
 drpResize.configure(width=13, bg="white")
+<<<<<<< Updated upstream
 chkGpu = Checkbutton(frameResize, text='Use GPU', onvalue=1, offvalue=0, bg="white")
 
 
+=======
+chkGpu = Checkbutton(frameResize, text='Use GPU', onvalue=3, offvalue=2, bg="white")
+
+btnCancel = Button(window, text='Cancel', font='Helvetica 10', width=12, height=1, command=cancel_convert, bg="white")
+>>>>>>> Stashed changes
 
 #Placing UI elements
 lblTitle.pack(fill=X)
@@ -219,8 +252,11 @@ frameLoad.pack(side = TOP, padx=10, anchor=W)
 txtLoadSize.pack(side=LEFT, padx=(0,88))
 txtFineSize.pack(side=LEFT)
 
+<<<<<<< Updated upstream
 #lblContinue.pack(side=TOP, pady=10, padx=10, anchor=W)
 chkContinue.pack(side=TOP, padx=10, pady=(15,0), anchor=W)
+=======
+>>>>>>> Stashed changes
 
 frameEpochLabel.pack(side = TOP, pady=10, padx=10, anchor=W)
 lblEpochCount.pack(side=LEFT, padx=(0,59))
@@ -229,11 +265,23 @@ frameEpoch.pack(side = TOP, padx=10, anchor=W)
 txtEpochCount.pack(side=LEFT, padx=(0,85))
 txtEpoch.pack(side=LEFT)
 
+<<<<<<< Updated upstream
+=======
+#lblContinue.pack(side=TOP, pady=10, padx=10, anchor=W)
+chkContinue.pack(side=TOP, padx=10, pady=(15,0), anchor=W)
+
+>>>>>>> Stashed changes
 lblResize.pack(side=TOP, pady=10, padx=10, anchor=W)
 frameResize.pack(side = TOP, padx=10, anchor=W)
 drpResize.pack(side=LEFT, padx=(0,20))
 chkGpu.pack(side=LEFT)
 
+<<<<<<< Updated upstream
+=======
+progressbar.pack(side = TOP, pady=(40,20), padx=10, anchor=W)
+btnCancel.pack(side = TOP, padx=(10,0), anchor=W)
+
+>>>>>>> Stashed changes
 outputBox.place(x=300, y=75)
 
 sys.stdout = StdoutRedirector( outputBox )
